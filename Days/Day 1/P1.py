88733fe97d03c0a3d3308dfc45f32c75e2a584b3
Antara Chavan <it.antara.chavan@gmail.com>
2026-07-09 16:21:38 +0530

    Example of Classes and Objects

---
+++ Days/Day 1/P1.py
@@ -0,0 +1,17 @@
+# Example of Classes and Objects
+ 
+class Dog: #Class
+    #Constructor
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        return f"{self.name} says woof! And it is {self.age} years old"
+
+#Creation of an  object
+myDog1 = Dog("Moti",5)
+myDog2 = Dog("Moochi",2)
+
+print(myDog1.bark())
+print(myDog2.bark())
