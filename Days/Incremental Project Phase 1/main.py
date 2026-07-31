2df4643658e715d1bf441fd9198f6ebc644dde83
Antara Chavan <it.antara.chavan@gmail.com>
2026-07-31 19:02:34 +0530

    Phase 3: Incremental Project

--- Days/Incremental Project Phase 1/main.py
+++ Days/Incremental Project Phase 1/main.py
@@ -1,5 +1,10 @@
 from models.product import Product
 from registry.registry import ProductRegistry
+from services.pricing_service import calculate_prices
+
+from reports.inventory import Inventory
+from reports.inventory import inventory_generator
+from reports.inventory import InventoryReport
 
 # Create products
 laptop = Product("Laptop", 55000, 10)
@@ -15,3 +20,34 @@ keyboard.display_details()
 
 # Display automatically registered product classes
 ProductRegistry.display_registered_products()
+
+products = [laptop,mouse,keyboard]
+price_details = calculate_prices(products)
+
+print("\n Price Report")
+
+for product,details in zip(products,price_details):
+    print(f"\n Product: {product.name}")
+    print(f"Original Price: {details['original']}")
+    print(f"Discounted Price: {details['discounted']}")
+    print(f"Tax: {details['tax']}")
+    print(f"Final Price: {details['final']}")
+
+print("Iterator Output")
+
+inventory = Inventory(products)
+
+for product in inventory:
+    print(product.name)
+
+print("Generator Output")
+for product in inventory_generator(products):
+    print(product.name)
+
+with InventoryReport("reports/inventory_report.txt") as report:
+    report.write("Inventory Report\n")
+    for product in inventory_generator(products):
+        report.write(
+            f"{product.name} | Rs.{product.price} | Qty: {product.quantity}\n"
+        )
+    print("\nInventory report generated successfully.")
